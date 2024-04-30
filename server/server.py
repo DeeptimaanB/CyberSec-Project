bc55dd276fdb77ea3de9f6a3c86d5e73c2b10847
Deeptimaan Banerjee <73385868+DeeptimaanB@users.noreply.github.com>
2024-04-29 23:21:25 -0600

    Update server.py

--- server/server.py
+++ server/server.py
@@ -3,6 +3,7 @@ import secrets
 import string
 import mysql.connector
 import time
+import random
 
 # server = "10.0.0.166"
 server = "192.168.10.1"
@@ -121,10 +122,50 @@ def update_salt_sql(user_id, salt):
         if 'connection' in locals():
             connection.close()     
 
+def update_salt_offset_sql(user_id, salt, offset_temp):
+    try:
+        # Establish connection to the MySQL database
+        connection = mysql.connector.connect(
+            host=server,
+            user="php_docker",
+            password="php_docker",
+            database="php_docker"
+        )
+
+        # Create a cursor object to execute SQL queries
+        cursor = connection.cursor()
+
+        # Define the SQL query to update the name based on user ID
+        sql_query = "UPDATE user_keys SET salt = %s, offset = %s WHERE id = %s"
+        cursor.execute(sql_query, (salt, offset_temp, user_id))
+
+        # Commit the transaction
+        connection.commit()
+
+        print("Salt updated successfully.")
+
+    except mysql.connector.Error as error:
+        # Rollback the transaction in case of error
+        print("Error while connecting to MySQL:", error)
+
+    finally:
+        # Close the cursor and connection
+        if 'cursor' in locals():
+            cursor.close()
+        if 'connection' in locals():
+            connection.close()     
+
+
 def update_salt(id):
     salt_text = generate_salt()
     update_salt_sql(id, salt_text)
     return salt_text
+
+def update_salt_offset(id):
+    salt_text = generate_salt()
+    offset_temp = str(random.randint(0, 63))
+    update_salt_offset_sql(id, salt_text, offset_temp)
+    return salt_text, int(offset_temp)
 
 zeroes = "0" * 96
 # Define your custom protocol class
@@ -164,18 +205,33 @@ def packet_handler(pkt):
         user_id = pkt[DEKX].user_id
         password = pkt[DEKX].password
         password = password.decode('utf-8')
-        password = extract_password(str(password), 3)
+        offset = pkt[DEKX].offset
+        if(offset == 97):
+            password = extract_password(str(password), 0)
+            
         result = search_user(int(user_id))
         if result:
-            user_id_text, password_md5, salt_text, offset_text = result 
-            temp_password = generate_md5(password_md5, salt_text)
-            if (int(user_id) == user_id_text and password == temp_password):
-                temp_salt = update_salt(str(user_id))
-                time.sleep(2)
-                custom_pkt = Ether(dst = "ff:ff:ff:ff:ff:ff", type=0xDE77)/DEKX(user_id=int(user_id), salt = temp_salt, offset = 98)
-                sendp(custom_pkt, iface="eth0")
-                time.sleep(0.5)  # Simulate some work being done
-                print("New Salt Sent " + temp_salt)
+            user_id_text, password_md5, salt_text, offset_text = result
+            password = extract_password(str(password), offset_text)
+            # print(str(user_id_text) + " "+ str(password_md5) + " " + str(salt_text) + " " +str(offset_text))
+            if(salt_text!=""):
+                temp_password = generate_md5(password_md5, salt_text)
+                if (int(user_id) == user_id_text and password == temp_password):
+                    temp_salt = update_salt(str(user_id))
+                    time.sleep(2)
+                    custom_pkt = Ether(dst = "ff:ff:ff:ff:ff:ff", type=0xDE77)/DEKX(user_id=int(user_id), salt = temp_salt, offset = 98)
+                    sendp(custom_pkt, iface="eth0")
+                    time.sleep(0.5)  # Simulate some work being done
+                    print("New Salt Sent " + temp_salt)
+            elif(salt_text==""):
+                if (int(user_id) == user_id_text and password == password):
+                    temp_salt, temp_offset = update_salt_offset(str(user_id))
+                    time.sleep(2)
+                    custom_pkt = Ether(dst = "ff:ff:ff:ff:ff:ff", type=0xDE77)/DEKX(user_id=int(user_id), salt = temp_salt, offset = temp_offset)
+                    sendp(custom_pkt, iface="eth0")
+                    time.sleep(0.5)  # Simulate some work being done
+                    print("New Salt Sent " + temp_salt)
+                
         else:
             print("No Data Found for userid : " + str(user_id))
 
