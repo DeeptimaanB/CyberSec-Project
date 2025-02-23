06c33cbd9e4e8e5fec12a6d7011a20d50bed9311
Shreyas Nitin Pujari <pujari.shreyas@gmail.com>
2025-02-23 06:21:13 +0000

    Added datetime field to the protocol

--- Protocol.py
+++ Protocol.py
@@ -1,13 +1,13 @@
 from scapy.all import *
 
-zeroes = "0" * 256
-salt_zeroes = "0" * 256
+sha_256_zeroes = "0" * 256
 # Define your custom protocol class
 class DEKX(Packet):
     name = "DEKX"
     fields_desc = [
         IntField("user_id", 0),
         IntField("offset", 259),
-        StrFixedLenField("password", zeroes, length=256),
-        StrFixedLenField("salt", salt_zeroes, length=256)
+        StrFixedLenField("password", sha_256_zeroes, length=256),
+        StrFixedLenField("salt", sha_256_zeroes, length=256),
+        StrFixedLenField("datetime", sha_256_zeroes, length=256)
     ]
